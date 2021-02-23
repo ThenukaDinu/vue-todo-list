0e14300d381cdf67d6031ff59a3e96a0f0d628e2
Thenukad Dinu <thenukadinu@gmail.com>
2021-02-23 22:45:30 +0530

    add some components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import Vuesax from 'vuesax';
+
+import 'vuesax/dist/vuesax.css';
+
+Vue.use(Vuesax);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
